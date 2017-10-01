fd0b1e4fe03d9ec15ca7439c8d82f4eacf6fde61
Sven Suursoho <sven@alt.ee>
2017-10-01 14:46:20 +0300

    Adjust gcc/clang versions for latest C++17 features

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,13 +1,13 @@
 case "${TRAVIS_OS_NAME}-${CC}" in
   linux-clang)
-    export CC=clang-3.9
-    export CXX=clang++-3.9
+    export CC=clang-5.0
+    export CXX=clang++-5.0
     ;;
 
   linux-gcc)
-    export CC=gcc-6
-    export CXX=g++-6
-    export GCOV=gcov-6
+    export CC=gcc-7
+    export CXX=g++-7
+    export GCOV=gcov-7
     ;;
 
   osx-clang)
