39fc5f95952001cb0914aba2032ac746f6335f6e
Sven Suursoho <sven@alt.ee>
2017-11-10 17:57:04 +0200

    Link with OpenSSL crypto library on Linux

--- cmake/linux.cmake
+++ cmake/linux.cmake
@@ -1,3 +1,12 @@
 #
 # Linux
 #
+
+# dependencies
+find_package(OpenSSL REQUIRED)
+list(FIND CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES OPENSSL_INCLUDE_DIR i)
+if(${i} EQUAL -1)
+  # not already in include path, add
+  include_directories(${OPENSSL_INCLUDE_DIR})
+endif()
+list(APPEND TURNER_DEPS_LIBS ${OPENSSL_CRYPTO_LIBRARY})
