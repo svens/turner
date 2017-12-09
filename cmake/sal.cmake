e1415662568532a9f654411e1d7b226837fd2c6d
Sven Suursoho <sven@alt.ee>
2017-12-09 19:15:02 +0200

    Include sal-0.25.0 as git submodule

--- cmake/sal.cmake
+++ cmake/sal.cmake
@@ -1,34 +1,14 @@
-find_path(SAL_INCLUDE_DIR sal/config.hpp)
-find_library(SAL_LIB sal)
+set(SAL_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/tps/sal")
 
-if(NOT SAL_INCLUDE_DIR OR NOT SAL_LIB)
-  message(STATUS "Building own sal")
-
-  include(ExternalProject)
-  ExternalProject_Add(sal
-    URL https://github.com/svens/sal/archive/v0.25.0.zip
-    URL_HASH SHA256=d3db7311392cef7ff0996e43e2b10fcd7a150aa4198a70da3d453fb27ab4d8b5
-    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/sal-0.25.0
-    CMAKE_ARGS
-      -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-      -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-      -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-      -Dsal_unittests=no
-      -Dsal_bench=no
-      -Dsal_docs=no
-    INSTALL_COMMAND ""
-    LOG_DOWNLOAD ON
-    LOG_CONFIGURE ON
-    LOG_BUILD ON
+if(NOT EXISTS ${SAL_INCLUDE_DIR}/sal/assert.hpp)
+  find_package(Git)
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} submodule update --init tps/sal
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
   )
-
-  # sal library
-  ExternalProject_Get_Property(sal binary_dir)
-  link_directories(${binary_dir})
-  set(SAL_LIB ${CMAKE_STATIC_LIBRARY_PREFIX}sal${CMAKE_STATIC_LIBRARY_SUFFIX})
-
-  # sal headers
-  ExternalProject_Get_Property(sal source_dir)
-  set(SAL_INCLUDE_DIR ${source_dir} ${binary_dir})
-  include_directories(${SAL_INCLUDE_DIR})
 endif()
+
+include_directories(${SAL_INCLUDE_DIR})
+
+set(sal_unittests OFF CACHE BOOL "Disable sal library testing")
+add_subdirectory(tps/sal)
