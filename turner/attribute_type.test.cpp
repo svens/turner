de7f0aea92e4b2033db88a4b042011a9bf447b80
Sven Suursoho <sven@alt.ee>
2023-04-30 15:02:48 +0300

    attribute_type: operator== & != for uint16_t

--- turner/attribute_type.test.cpp
+++ turner/attribute_type.test.cpp
@@ -5,24 +5,25 @@ namespace {
 
 using namespace turner_test;
 
-struct Protocol
-{
-	struct attribute_value_type { };
-	static constexpr auto a1 = turner::attribute<Protocol, attribute_value_type, 0x0001>;
-	static constexpr auto a2 = turner::attribute<Protocol, attribute_value_type, 0x8001>;
-};
-
 TEST_CASE("attribute_type")
 {
-	static_assert(std::is_same_v<typename decltype(Protocol::a1)::protocol_type, Protocol>);
-	static_assert(Protocol::a1.type == 0x0001);
-	static_assert(Protocol::a1.is_comprehension_required);
-	static_assert(!Protocol::a1.is_comprehension_optional);
+	__turner_check(std::is_same_v<typename decltype(test_protocol::a0)::protocol_type, test_protocol>);
+	__turner_check(test_protocol::a0 == 0x0001);
+	__turner_check(test_protocol::a0 != 0x0002);
+	__turner_check(test_protocol::a0.is_comprehension_required);
+	__turner_check(!test_protocol::a0.is_comprehension_optional);
 
-	static_assert(std::is_same_v<typename decltype(Protocol::a2)::protocol_type, Protocol>);
-	static_assert(Protocol::a2.type == 0x8001);
-	static_assert(!Protocol::a2.is_comprehension_required);
-	static_assert(Protocol::a2.is_comprehension_optional);
+	__turner_check(std::is_same_v<typename decltype(test_protocol::a1)::protocol_type, test_protocol>);
+	__turner_check(test_protocol::a1 == 0x0002);
+	__turner_check(test_protocol::a1 != 0x0001);
+	__turner_check(test_protocol::a1.is_comprehension_required);
+	__turner_check(!test_protocol::a1.is_comprehension_optional);
+
+	__turner_check(std::is_same_v<typename decltype(test_protocol::a2)::protocol_type, test_protocol>);
+	__turner_check(test_protocol::a2 == 0x8001);
+	__turner_check(test_protocol::a2 != 0x8002);
+	__turner_check(!test_protocol::a2.is_comprehension_required);
+	__turner_check(test_protocol::a2.is_comprehension_optional);
 }
 
 } // namespace
